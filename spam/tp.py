1f344258c070a985e0bba457110d7506c34bdd8f
Damian Aleman <damianealeman@gmail.com>
2016-09-24 14:35:30 -0300

    score

--- spam/tp.py
+++ spam/tp.py
@@ -5,9 +5,14 @@
 # Segundo cuatrimestre 2016
 """Modulo de TP Spam Detector.
 
+Cargar datos:
+	python tp.py File
+Entrenar un modelo:
+	python tp.py [Gsearch] metodo [cv]
+Predecir:
+	python tp.py Test metodo base
 
-python tp.py [Gsearch] metodo cv [parametros]
-#metodos = Dtree, Rforest, Knn, Nbayes, Svc 
+	metodos = Dtree, Rforest, Knn, Nbayes, Svc 
 
 """
 import sys
@@ -17,6 +22,7 @@ import numpy as np
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.cross_validation import cross_val_score
+from sklearn.cross_validation import train_test_split
 from sklearn import datasets
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
@@ -25,6 +31,8 @@ from sklearn.grid_search import GridSearchCV
 from sklearn.feature_selection import RFE
 from sklearn.feature_selection import RFECV
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import ( accuracy_score, precision_score, recall_score, f1_score )
+import pickle
 
 def count_mm(txt): return txt.count("mailman.enron.com")
 def count_by(txt): return txt.count("by")
@@ -127,6 +135,84 @@ def count_title(txt): return sum([c.istitle() for c in txt])
 def lprom(txt): return sum([len(c) for c in txt])/(len(txt))
 def lmax(txt): return max([len(c) for c in txt])
 
+def cargar_mails():		
+	# Leo los mails
+	print 'Cargando ham'
+	ham_txt = json.load(open('./dataset_dev/ham_dev.json'))
+	print 'Cargando spam'
+	spam_txt = json.load(open('./dataset_dev/spam_dev.json'))
+
+	df = pd.DataFrame(ham_txt+spam_txt, columns=['text'])
+	df['class'] = ['ham' for _ in range(len(ham_txt))]+['spam' for _ in range(len(spam_txt))]
+	return df
+
+def guardar_modelo(metodo):
+	str_file = metodo + '_entrenado.pickle'	
+	fout = open(str_file,'w')
+	pickle.dump(clf,fout)
+	fout.close()
+	return
+
+def cargando_atributos(df):
+
+	dnames = ['len','count_td','count_by','count_ESMTP','count_menora', 'count_n','count_upper','count_mm','count_ref','count_guionba'
+,'count_menorp','count_px','count_from','count_id', 'count_spaces','count_cum','count_viagra','count_sex','count_vagina','count_penis'
+,'count_money','count_earn','count_free','count_now','count_help','count_excl','count_preg','count_dol','count_dollar','count_dollars'
+,'count_1','count_2','count_3','count_4','count_5','count_6','count_7','count_8','count_9','count_0'
+,'count_work','count_arr','count_hash','count_and','count_apare','count_acor','count_plus','count_mult','count_porc','count_equal'
+,'count_dot','count_dotc','count_apos','count_com','count_send','count_menor','count_dosp','count_offer','count_deal','count_join'
+,'count_vote','count_weig','count_lose','count_menos','count_sombrero','count_aparen','count_cparen','count_num','count_title','count_helvetica'
+,'count_arial','count_nigeria','count_win','count_FREE','count_VIAGRA','count_SEX','count_VAGINA','count_PENIS','count_MONEY','count_EARN'
+,'count_NOW','count_html','count_pill','count_hours','lprom','lmax','count_solid','count_font','count_tr','count_microsoft'
+,'count_HTML','count_0600','count_2002','count_nahou','count_with','count_your','count_0800','count_unv','count_sat','count_sun'
+,'count_huge']
+
+	dfuncs = [len,count_td,count_by,count_ESMTP,count_menora, count_n,count_upper,count_mm,count_ref,count_guionba
+,count_menorp,count_px,count_from,count_id, count_spaces,count_cum,count_viagra,count_sex,count_vagina,count_penis
+,count_money,count_earn,count_free,count_now,count_help,count_excl,count_preg,count_dol,count_dollar,count_dollars
+,count_1,count_2,count_3,count_4,count_5,count_6,count_7,count_8,count_9,count_0
+,count_work,count_arr,count_hash,count_and,count_apare,count_acor,count_plus,count_mult,count_porc,count_equal
+,count_dot,count_dotc,count_apos,count_com,count_send,count_menor,count_dosp,count_offer,count_deal,count_join
+,count_vote,count_weig,count_lose,count_menos,count_sombrero,count_aparen,count_cparen,count_num,count_title,count_helvetica
+,count_arial,count_nigeria,count_win,count_FREE,count_VIAGRA,count_SEX,count_VAGINA,count_PENIS,count_MONEY,count_EARN
+,count_NOW,count_html,count_pill,count_hours,lprom,lmax,count_solid,count_font,count_tr,count_microsoft
+,count_HTML,count_0600,count_2002,count_nahou,count_with,count_your,count_0800,count_unv,count_sat,count_sun
+,count_huge]
+
+	start_time = time.time()
+	print "Cargando atributos"
+	for i in range(len(dnames)):
+#		print str(i) + " " + dnames[i]
+		df[dnames[i]] = map(dfuncs[i], df.text)
+#		print("--- %s seconds ---" % (time.time() - start_time))
+	return df, dnames
+
+def preparo_datos(df, dnames):
+	# Preparo data para clasificar
+	X = df[dnames].values
+	y = df['class']
+
+	np.save('trainX', X)
+	np.save('trainy', y)
+	return (X,y)
+
+def predecir(metodo, test):
+	df = pd.DataFrame(test, columns=['text'])
+	df, dnames = cargando_atributos(df)
+	clf = pickle.load(open(metodo + '_entrenado.pickle'))
+	predy = list(clf.predict(df[dnames].values))
+	f = open('testy.txt', 'r')
+	testy = f.read().split(',')
+	f.close()
+	print testy
+	print predy
+	testn = map(lambda x : ( 1 if x == 'ham' else 0 ), testy)
+	predn = map(lambda x : ( 1 if x == 'ham' else 0 ), predy)
+	print("\tPrecision: %1.3f" % precision_score(testn, predn))
+	print("\tRecall: %1.3f" % recall_score(testn, predn))
+	print("\tF1: %1.3f\n" % f1_score(testn, predn))
+	print("\tAccuaracy: %1.3f\n" % accuracy_score(testn, predn))
+
 if __name__ == '__main__':
 	cv = 10
 	gs = False
@@ -140,7 +226,21 @@ if __name__ == '__main__':
 				print u'Método inválido'
 				exit()
 			gs = True
-			n = 3
+			n = n + 1
+		elif metodo == 'Test':
+			if len(sys.argv) > n:
+				metodo = sys.argv[n]
+			else:
+				print u'Método inválido'
+				exit()
+			n = n + 1
+			if len(sys.argv) > n:
+				test = json.load(open(sys.argv[n]))
+			else:
+				print u'¿Cuál es la base de test?'
+				exit()
+			predecir(metodo, test)
+			exit()
 		if len(sys.argv) > n:
 			cv = int(sys.argv[n])
 	else:
@@ -158,65 +258,39 @@ if __name__ == '__main__':
 	elif metodo == 'Svc':
 		pass
 	elif metodo == 'File':
-
-		# Leo los mails
-		print 'Cargando ham'
-		ham_txt = json.load(open('./dataset_dev/ham_dev.json'))
-		print 'Cargando spam'
-		spam_txt = json.load(open('./dataset_dev/spam_dev.json'))
-
-		df = pd.DataFrame(ham_txt+spam_txt, columns=['text'])
-		df['class'] = ['ham' for _ in range(len(ham_txt))]+['spam' for _ in range(len(spam_txt))]
-
-		dnames = ['len','count_td','count_by','count_ESMTP','count_menora', 'count_n','count_upper','count_mm','count_ref','count_guionba'
-,'count_menorp','count_px','count_from','count_id', 'count_spaces','count_cum','count_viagra','count_sex','count_vagina','count_penis'
-,'count_money','count_earn','count_free','count_now','count_help','count_excl','count_preg','count_dol','count_dollar','count_dollars'
-,'count_1','count_2','count_3','count_4','count_5','count_6','count_7','count_8','count_9','count_0'
-,'count_work','count_arr','count_hash','count_and','count_apare','count_acor','count_plus','count_mult','count_porc','count_equal'
-,'count_dot','count_dotc','count_apos','count_com','count_send','count_menor','count_dosp','count_offer','count_deal','count_join'
-,'count_vote','count_weig','count_lose','count_menos','count_sombrero','count_aparen','count_cparen','count_num','count_title','count_helvetica'
-,'count_arial','count_nigeria','count_win','count_FREE','count_VIAGRA','count_SEX','count_VAGINA','count_PENIS','count_MONEY','count_EARN'
-,'count_NOW','count_html','count_pill','count_hours','lprom','lmax','count_solid','count_font','count_tr','count_microsoft'
-,'count_HTML','count_0600','count_2002','count_nahou','count_with','count_your','count_0800','count_unv','count_sat','count_sun'
-,'count_huge']
-
-		dfuncs = [len,count_td,count_by,count_ESMTP,count_menora, count_n,count_upper,count_mm,count_ref,count_guionba
-,count_menorp,count_px,count_from,count_id, count_spaces,count_cum,count_viagra,count_sex,count_vagina,count_penis
-,count_money,count_earn,count_free,count_now,count_help,count_excl,count_preg,count_dol,count_dollar,count_dollars
-,count_1,count_2,count_3,count_4,count_5,count_6,count_7,count_8,count_9,count_0
-,count_work,count_arr,count_hash,count_and,count_apare,count_acor,count_plus,count_mult,count_porc,count_equal
-,count_dot,count_dotc,count_apos,count_com,count_send,count_menor,count_dosp,count_offer,count_deal,count_join
-,count_vote,count_weig,count_lose,count_menos,count_sombrero,count_aparen,count_cparen,count_num,count_title,count_helvetica
-,count_arial,count_nigeria,count_win,count_FREE,count_VIAGRA,count_SEX,count_VAGINA,count_PENIS,count_MONEY,count_EARN
-,count_NOW,count_html,count_pill,count_hours,lprom,lmax,count_solid,count_font,count_tr,count_microsoft
-,count_HTML,count_0600,count_2002,count_nahou,count_with,count_your,count_0800,count_unv,count_sat,count_sun
-,count_huge]
-
-		start_time = time.time()
-		print "Cargando atributos"
-		for i in range(len(dnames)):
-			print str(i) + " " + dnames[i]
-			df[dnames[i]] = map(dfuncs[i], df.text)
-			print("--- %s seconds ---" % (time.time() - start_time))
-	
-		# Preparo data para clasificar
-		X = df[dnames].values
-		y = df['class']
-
-		np.save('testX', X)
-		np.save('testy', y)
+		df = cargar_mails()
+		train, test = train_test_split(df, test_size = 0.2)
+		train, dnames = cargando_atributos(train)
+		(X,y) = preparo_datos(train, dnames)
+		test_file = "["
+		for i in test['text']:
+			test_file += "\"" + i.replace("\r","\\r").replace("\n","\\n").replace("\t","\\t").replace("\"","\\\"") + "\","
+		test_file = test_file[:len(test_file)-1] # elimino la última coma
+		test_file += "]"
+		f = open('test.json', 'w')
+		f.write(test_file)
+		f.close()
+		test_file = ""
+		for i in test['class']:
+			test_file += i + ","
+		test_file = test_file[:len(test_file)-1] # elimino la última coma
+		f = open('testy.txt', 'w')
+		f.write(test_file)
+		f.close()
 		exit()
 	else:
 		print u'Método inválido'
 		exit()
 
-	X = np.load('testX.npy')	
-	y = np.load('testy.npy')
+	X = np.load('trainX.npy')	
+	y = np.load('trainy.npy')
 
 	if metodo == 'Dtree':
 		clf = DecisionTreeClassifier()
+		clf.fit(X, y)
 	elif metodo == 'Rforest':
 		clf = RandomForestClassifier()
+		clf.fit(X, y)
 	elif metodo == 'Knn':
 		clf = KNeighborsClassifier()
 		clf.fit(X, y)
@@ -236,5 +310,10 @@ if __name__ == '__main__':
 		clf = GridSearchCV(clf, param_grid=param_grid,n_jobs=-1)
 		clf.fit(X, y)
 
+	guardar_modelo(metodo)
+
 	res = cross_val_score(clf, X, y, cv=cv)
 	print np.mean(res), np.std(res)
+
+
+
